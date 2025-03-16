0006b4473157a6470f4b86dc377a3b471993cb5c
williamhhhh <spikejoe329@gmail.com>
2025-03-16 15:05:01 +1100

    uvimpact with graph done

--- src/views/UVimpactPage.vue
+++ src/views/UVimpactPage.vue
@@ -2,7 +2,7 @@
   
     <div class="card mb-2">
       <div class="row g-0">
-        <div class="col-md-6">
+        <div class="col-md-5">
           <div :class="card-body">
             <h5 class="card-title">Melanoma & UV Impact : The Hidden Danger in Sun Exposure</h5>
             <h6 class="card-subtitle mb-2 text-muted" style="font-weight: normal;">Stay Informed, Stay Protected</h6>
@@ -10,11 +10,13 @@
             <p class="card-text" style="font-weight: 300;">Melanoma is the deadliest form of skin cancer, with over 18,000 new cases expected in Australia in 2023, making up 11% of all cancer diagnoses. Despite a 94% survival rate over five years, early detection and prevention are crucial. UV exposure is the primary cause, increasing the risk for everyone, regardless of skin type. With 1 in 17 Australians likely to develop melanoma by age 85 and over 1,300 deaths projected this year, protecting your skin from the sun isn’t optional—it’s essential.</p>
           </div>
         </div>
-          <div class="col-md-6">
-            <img src="https://i.imgur.com/DM1sHjq.png" class="img-fluid rounded-start h-100 w-100" style="object-fit: contain;" alt="...">
+          <div class="col-md-7">
+            <img v-if="linePlotData" :src="linePlotData" class="img-fluid rounded-start h-100 w-100" style="object-fit: contain;" alt="...">
         </div>
       </div>
     </div>
+
+    <!-- https://i.imgur.com/DM1sHjq.png -->
 
     <div class="card mb-3">
       <div class="row g-0">
@@ -36,6 +38,28 @@
   
 <script setup>
 
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const linePlotData = ref(null)
+
+const fetchImage = async () => {
+  try {
+    const response = await axios.get("https://0ewwm7syga.execute-api.ap-southeast-2.amazonaws.com/plot_line", {
+      responseType: 'json'
+    })
+    console.log(response.data)
+
+    linePlotData.value = `data:image/png;base64,${response.data.image}`;
+    
+  } catch (error) {
+    console.error("Error fetching image", error)
+  }
+}
+
+onMounted( () => {
+  fetchImage()
+})
 </script>
   
 <style scoped>
